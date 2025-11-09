f31ad2bc87481f980f56e18cabd00673eb756014
kukx234 <kukx234@gmail.com>
2025-11-09 19:35:10 +0100

    -

--- db/migrate/20251109114724_create_users.rb
+++ db/migrate/20251109114724_create_users.rb
@@ -4,7 +4,7 @@ class CreateUsers < ActiveRecord::Migration[8.1]
       t.string :first_name
       t.string :last_name
       t.string :email
-      #TODO add password field for login | register
+      # TODO add password field for login | register
       t.timestamps
     end
   end
